f4a3f1f746b2aeff30e04a2439820419accadfe9
Zephyrtoria <2594379136@qq.com>
2025-02-27 19:39:26 +0800

    v0.2用户上线功能

--- server.go
+++ server.go
@@ -3,18 +3,28 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	// 在线用户列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	// 消息广播的channel
+	Message chan string
 }
 
 // 创建一个Server对象
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
 }
@@ -22,6 +32,35 @@ func NewServer(ip string, port int) *Server {
 // 当前链接的业务
 func (s *Server) Handler(conn net.Conn) {
 	fmt.Println("链接建立成功")
+	// 用户上线，将用户加入到OnlineMap中
+	user := NewUser(conn)
+	s.mapLock.Lock()
+	s.OnlineMap[user.Name] = user
+	s.mapLock.Unlock()
+
+	// 广播当前用户上线信息
+	s.BroadCast(user, "已上线")
+
+	// 当前handler阻塞
+}
+
+// 广播信息的方法
+func (s *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	s.Message <- sendMsg
+}
+
+// 监听Message广播消息channel的goroutine，一旦有消息就发送给全部的在线User
+func (s *Server) ListenMessage() {
+	for {
+		msg := <-s.Message
+		// 将msg发送给全部的在线User
+		s.mapLock.Lock()
+		for _, cli := range s.OnlineMap {
+			cli.C <- msg
+		}
+		s.mapLock.Unlock()
+	}
 }
 
 // 启动服务器的接口
@@ -35,6 +74,8 @@ func (s *Server) Start() {
 
 	// close socket listener
 	defer listener.Close()
+	// 启动监听Message的goroutine
+	go s.ListenMessage()
 
 	for {
 		// accept
